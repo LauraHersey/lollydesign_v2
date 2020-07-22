e13ae6016c97753599b8f7f6137495e303dccf93
David Hersey <david.hersey@holidayextras.com>
2020-07-22 17:23:00 +0100

    Extend event tracking

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from "@reach/router"
+import trackingHelpers from '../helpers/trackingHelpers'
 
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -9,15 +10,15 @@ class Navigation extends React.Component {
     return (
       <header>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
-          <Link to="/">Lollydesign</Link>
+          <Link to="/" onClick={e => trackingHelpers.eventTrack('Nav Link', 'Click', 'Home')}>Lollydesign</Link>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto navbar-links">
               <ul>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/project">Projects</Link></li>
-                <li><Link to="/services">Our Services</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/about" onClick={e => trackingHelpers.eventTrack('Nav Link', 'Click', 'About')}>About</Link></li>
+                <li><Link to="/project" onClick={e => trackingHelpers.eventTrack('Nav Link', 'Click', 'Projects')}>Projects</Link></li>
+                <li><Link to="/services" onClick={e => trackingHelpers.eventTrack('Nav Link', 'Click', 'Services')}>Our Services</Link></li>
+                <li><Link to="/contact" onClick={e => trackingHelpers.eventTrack('Nav Link', 'Click', 'Contact')}>Contact</Link></li>
               </ul>
             </Nav>
           </Navbar.Collapse>
